036def3c8b7f03e46e84d7febf0a80a5ce43960e
leven2000 <38410880+leven2000@users.noreply.github.com>
2022-02-08 16:19:13 -0300

    Update index.js

--- EntregaFinal/index.js
+++ EntregaFinal/index.js
@@ -10,6 +10,8 @@ const peliculas = document.querySelectorAll('.pelicula');
 const flechaIzquierda = document.getElementById('flecha-izquierda');
 const flechaDerecha = document.getElementById('flecha-derecha');
 
+$(`#cuenta`).replaceWith(`<a id="cuenta">${usuario}</a>`)
+
 // ? ----- ----- Event Listener para la flecha derecha. ----- -----//
 flechaDerecha.addEventListener('click', () => {
 	fila.scrollLeft += fila.offsetWidth;
